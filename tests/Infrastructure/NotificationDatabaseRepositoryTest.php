5a0ffbca35ab208d7a688fa1f2a655253bca0d6d
Albert Barreto <albert.barreto@outlook.com>
2020-03-01 20:00:26 -0300

    phpunit test

--- tests/Infrastructure/NotificationDatabaseRepositoryTest.php
+++ tests/Infrastructure/NotificationDatabaseRepositoryTest.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace Tests\Infrastructure\User;
+namespace Tests\Infrastructure\Notification;
 
+use \PDO;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 use Deezer\Infrastructure\Notification\NotificationDatabaseRepository;
-use Deezer\Infrastructure\Notification\NotificationDatabaseRepository\PDO;
 
-class NotificationDatabaseRepositoryTest extends TestCase
-{
+class NotificationDatabaseRepositoryTest extends TestCase {
 
     /** @var Logger */
     private $logger;
@@ -16,12 +15,100 @@ class NotificationDatabaseRepositoryTest extends TestCase
     /** @var NotificationDatabaseRepository */
     private $notificationDatabaseRepository;
 
-    public function setUp(): void
-    {
-        $this->notificationDatabaseRepository = new NotificationDatabaseRepository (
-            $this->createMock(\PDO::class),
-            $this->createMock(\Logger::class)
+    public function setUp(): void {
+        $this->PDO = $this->getConnection();
+        $this->createTable();
+        $this->populateTable();
+
+        $this->notificationDatabaseRepository = new NotificationDatabaseRepository($this->PDO, $this->logger);
+    }
+
+    public function tearDown(): void {
+        unset($this->notificationDatabaseRepository);
+        unset($this->PDO);
+    }
+
+    public function testFindById() {
+        $id = 1;
+
+        $result = $this->notificationDatabaseRepository->findById($id);
+        $this->assertInternalType(
+            'array',
+            $result,
+            'The result should always be an array.'
+        );
+        $this->assertEquals(
+            $id,
+            $result['id'],
+            'The id key/value of the result for id should be equal to the id.'
+        );
+        $this->assertEquals(
+            1,
+            $result['id'],
+            'The id key/value of the result for name should be equal to 1.'
         );
     }
-    
+
+    public function testFindByIdMock() {
+        $id = 1;
+
+        $PDOStatement = $this->getMockBuilder('\PDOStatement')
+            ->setMethods(['execute', 'fetch'])
+            ->getMock();
+
+        $PDOStatement->expects($this->once())
+            ->method('execute')
+            ->with([$id])
+            ->will($this->returnSelf());
+        $PDOStatement->expects($this->once())
+            ->method('fetch')
+            ->with($this->anything())
+            ->will($this->returnValue(1));
+
+        $PDO = $this->getMockBuilder('\PDO')
+            ->setMethods(['prepare'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $PDO->expects($this->once())
+            ->method('prepare')
+            ->with($this->stringContains('SELECT * FROM'))
+            ->willReturn($PDOStatement);
+
+        $notificationDatabaseRepository = new NotificationDatabaseRepository($PDO, $this->logger);
+
+        $output = $notificationDatabaseRepository->findById($id);
+
+        $this->assertEquals(
+            1,
+            $output,
+            'The output for the mocked instance of the PDO and PDOStatment should produce the int 1.'
+        );
+    }
+
+    protected function getConnection() {
+        return new PDO('sqlite::memory:');
+    }
+
+    protected function createTable() {
+        $query = "
+		CREATE TABLE `notification` (
+			`id`	     INTEGER,
+			`user_id`	 INTEGER,
+			`message_id` INTEGER,
+			`status`     INTEGER,
+			PRIMARY KEY(`id`)
+		);
+		";
+        $this->PDO->query($query);
+    }
+
+    protected function populateTable() {
+        $query = "
+		INSERT INTO `notification` VALUES (1, 1, 1, 1);
+		INSERT INTO `notification` VALUES (2, 2, 4, 0);
+		";
+        $this->PDO->query($query);
+    }
 }
+
--- tests/Infrastructure/UserDatabaseRepositoryTest.php
+++ tests/Infrastructure/UserDatabaseRepositoryTest.php
@@ -1,37 +1,116 @@
 <?php
 
-namespace Tests\Infrastructure\User;
+namespace Tests\Infrastructure\Notification;
 
+
+use \PDO;
 use Monolog\Logger;
-use Deezer\Domain\User\User;
 use PHPUnit\Framework\TestCase;
 use Deezer\Infrastructure\User\UserDatabaseRepository;
-use Deezer\Infrastructure\User\UserDatabaseRepository\PDO;
 
-class UserDatabaseRepositoryTest extends TestCase
-{
+class UserDatabaseRepositoryTest extends TestCase {
 
     /** @var Logger */
     private $logger;
 
-    /** @var User */
-    private $user;
-
     /** @var UserDatabaseRepository */
     private $userDatabaseRepository;
 
-    public function setUp(): void
-    {
-        $this->user = new User();
-        $this->userDatabaseRepository = new UserDatabaseRepository(
-            $this->createMock(\PDO::class),
-            $this->createMock(\Logger::class)
+    public function setUp(): void {
+        $this->PDO = $this->getConnection();
+        $this->createTable();
+        $this->populateTable();
+
+        $this->userDatabaseRepository = new UserDatabaseRepository($this->PDO, $this->logger);
+    }
+
+    public function tearDown(): void {
+        unset($this->userDatabaseRepository);
+        unset($this->PDO);
+    }
+
+    public function testFindById() {
+        $id = 1;
+
+        $result = $this->userDatabaseRepository->findById($id);
+        $this->assertInternalType(
+            'array',
+            $result,
+            'The result should always be an array.'
+        );
+        $this->assertEquals(
+            $id,
+            $result['id'],
+            'The id key/value of the result for id should be equal to the id.'
+        );
+        $this->assertEquals(
+            'Deezer',
+            $result['name'],
+            'The id key/value of the result for name should be equal to `Deezer`.'
         );
     }
 
-    public function testFindAll()
-    {
+    public function testFindByIdMock() {
+        $id = 1;
 
+        $PDOStatement = $this->getMockBuilder('\PDOStatement')
+            ->setMethods(['execute', 'fetch'])
+            ->getMock();
+
+        $PDOStatement->expects($this->once())
+            ->method('execute')
+            ->with([$id])
+            ->will($this->returnSelf());
+        $PDOStatement->expects($this->once())
+            ->method('fetch')
+            ->with($this->anything())
+            ->will($this->returnValue(1));
+
+        $PDO = $this->getMockBuilder('\PDO')
+            ->setMethods(['prepare'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $PDO->expects($this->once())
+            ->method('prepare')
+            ->with($this->stringContains('SELECT * FROM'))
+            ->willReturn($PDOStatement);
+
+        $notificationDatabaseRepository = new UserDatabaseRepository($PDO, $this->logger);
+
+        $output = $notificationDatabaseRepository->findById($id);
+
+        $this->assertEquals(
+            'Deezer',
+            $output,
+            'The output for the mocked instance of the PDO and PDOStatment should produce the string `Deezer`.'
+        );
     }
 
+    protected function getConnection() {
+        return new PDO('sqlite::memory:');
+    }
+
+    protected function createTable() {
+        $query = "
+		CREATE TABLE `user` (		        
+            `id`	    INTEGER,
+			`name`	    TEXT,
+			`email`     TEXT,
+			`password`  TEXT,
+			`type`      TEXT,
+			PRIMARY KEY(`id`)
+		);
+		";
+        $this->PDO->query($query);
+    }
+
+    protected function populateTable() {
+        $query = "
+		    INSERT INTO `user` VALUES (1, 'Dezzer', 'deezer@deezer.com', '202cb962ac59075b964b07152d234b70', 'administrator');
+		    INSERT INTO `user` VALUES (2, 'user 1', 'user1@deezer.com', '202cb962ac59075b964b07152d234b70', 'user');
+		";
+        $this->PDO->query($query);
+    }
 }
+
